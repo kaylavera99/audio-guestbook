acbad49d126c58442390402b8192cdd5b32a24e3
kaylavera99 <kaylavd99@gmail.com>
2025-05-04 13:33:34 -0400

    Upload to S3 functionality
    Added functionality that will start and stop recording, and upon recording stopping, the recording will be saved with a unique name, as a WAV file to an S3 bucket

--- app.py
+++ app.py
@@ -1,11 +1,36 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, jsonify
+import boto3
+import os
+import uuid
+from datetime import datetime
 app = Flask(__name__)
 
-@app.route("/")
+s3 = boto3.client('s3')
 
-def index():
+BUCKET_NAME = "kayla-audio-guestbook"
+
+@app.route("/")
+def home():
     return render_template("index.html")
+
+@app.route("/save", methods=["POST"])
+def save():
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    unique_id = uuid.uuid4().hex[:6]
+    file = request.files['audio_data']
+    filename =  f"guest_message_{timestamp}_{unique_id}.wav"
+    local_path = os.path.join("uploads", filename)
+    file.save(local_path)
+
+
+    try:
+        s3.upload_file(local_path, BUCKET_NAME, filename)
+        s3_url = f"https://{BUCKET_NAME}.s3.amazonaws.com/{filename}"
+        return jsonify({"message": "File uploaded successfully", "url": s3_url}), 200
+    except Exception as e:
+        return jsonify({"error":str(e)}), 500
+
+    
 
 
 if __name__ == "__main__":
